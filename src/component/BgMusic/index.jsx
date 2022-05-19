78d96ead2d91f112b6c71df36c0219e802f85ef0
chiyao <chiyao.1402@bytedance.com>
2022-05-19 18:17:23 +0800

    1

---
+++ src/component/BgMusic/index.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useRef, useState } from "react";
+
+import './index.less'
+const Music = (props) => {
+  const BGM = useRef(null)
+  const [playing,setPlay] = useState(false)
+  
+  const handleChildClick = useCallback(()=>{
+    if(!BGM.current){
+      BGM.current = new Audio();
+      BGM.current.src = props.src;
+      BGM.current.loop=true;
+      BGM.current.play()
+      setPlay(true)
+    }
+  },[props.src])
+
+  const handleControl = useCallback((playing)=>{
+    const {current:bgm} = BGM
+    console.log(BGM)
+    if(playing){
+      bgm.pause()
+      setPlay(false)
+    }else{
+      bgm.play()
+      setPlay(true)
+    }
+  },[])
+
+  return (
+  <div className="bg-music"
+    onClick={handleChildClick}>
+    {props.children}
+    <div className="controller"
+      onClick={()=>handleControl(playing)}>
+      {playing?<>1</>:<>2</>}
+    </div>
+  </div>
+  )
+}
+
+export default Music
